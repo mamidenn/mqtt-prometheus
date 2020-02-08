8a4911d353eeb40a99cb458739be7a37c5cad750
Martin Dennhardt <martin@dennhardt.de>
2020-02-08 20:54:55 +0100

    Add naive implementation of value pushing

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -10,7 +12,14 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic+" "+str(msg.payload))    
+    if(msg.topic == 'hab/livingroom/temperature'):        
+        t.set(float(msg.payload))
+    if(msg.topic == 'hab/livingroom/humidity'):        
+        h.set(float(msg.payload))
+    push_to_gateway('rasputin:32768', job='livingroom', registry=registry)
+
+    
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -18,6 +27,9 @@ client.on_message = on_message
 
 client.connect("rasputin", 1883, 60)
 
+registry = CollectorRegistry()
+t = Gauge('temperature', 'Current temperature', registry=registry)
+h = Gauge('humidity', 'Current humididty', registry=registry)
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
